355152ba34256d933c0467b24f3375cb827b0955
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-12-12 11:28:16 +0100

    naming changed and typescript

---
+++ day01_prepareGifts/day01.ts
@@ -0,0 +1,20 @@
+function prepareGifts(gifts: number[]): number[] {
+  const res = [...new Set(gifts)];
+  res.sort((a, b) => a - b);
+
+  return res;
+}
+
+//Examples
+const gifts1: number[] = [3, 1, 2, 3, 4, 2, 5];
+const preparedGifts1 = prepareGifts(gifts1);
+console.log(preparedGifts1); // [1, 2, 3, 4, 5]
+
+const gifts2: number[] = [6, 5, 5, 5, 5];
+const preparedGifts2 = prepareGifts(gifts2);
+console.log(preparedGifts2); // [5, 6]
+
+const gifts3: number[] = [];
+const preparedGifts3 = prepareGifts(gifts3);
+console.log(preparedGifts3); // []
+// No hay regalos, la lista queda vacía
