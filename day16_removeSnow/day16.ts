086b514196a558769f9c3474fea8ee780f42f97f
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-12-17 11:28:05 +0100

    day 16 done

---
+++ day16_removeSnow/day16.ts
@@ -0,0 +1,21 @@
+function removeSnow(s: string): string {
+  const pile = [];
+  for (const c of s) {
+    pile.at(-1) === c ? pile.pop() : pile.push(c);
+  }
+  return pile.join("");
+}
+
+// Examples
+console.log(removeSnow("zxxzoz")); // -> "oz"
+// 1. Eliminamos "xx", quedando "zzoz"
+// 2. Eliminamos "zz", quedando "oz"
+
+console.log(removeSnow("abcdd")); // -> "abc"
+// 1. Eliminamos "dd", quedando "abc"
+
+console.log(removeSnow("zzz")); // -> "z"
+// 1. Eliminamos "zz", quedando "z"
+
+console.log(removeSnow("a")); // -> "a"
+// No hay montículos repetidos
